63688896fb9facd526f9be689f18f8645bdb7512
Terry <terry.h@outlook.com>
2015-09-15 20:08:45 -0500

    init

---
+++ imageviewer.h
@@ -0,0 +1,50 @@
+#ifndef IMAGEVIEWER_H
+#define IMAGEVIEWER_H
+
+#include <QMainWindow>
+
+class QAction;
+class QLabel;
+class QMenu;
+class QScrollArea;
+class QScrollBar;
+
+class ImageViewer : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    ImageViewer();
+    bool loadFile(const QString &);
+
+private slots:
+    void open();
+    void zoomIn();
+    void zoomOut();
+    void normalSize();
+    void fitToWindow();
+
+private:
+    void createActions();
+    void createMenus();
+    void updateActions();
+    void scaleImage(double factor);
+    void adjustScrollBar(QScrollBar *scrollBar, double factor);
+
+    QLabel *imageLabel;
+    QScrollArea *scrollArea;
+    double scaleFactor;
+
+    QAction *openAct;
+    QAction *exitAct;
+    QAction *zoomInAct;
+    QAction *zoomOutAct;
+    QAction *normalSizeAct;
+    QAction *fitToWindowAct;
+
+    QMenu *fileMenu;
+    QMenu *viewMenu;
+    QMenu *helpMenu;
+};
+
+#endif
